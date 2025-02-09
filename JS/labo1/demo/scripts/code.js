564c7013a5c59994f2d30c79abcf2e900da1c1ff
Adrian-Jaron <adrian.jaron@student.vives.be>
2025-02-09 20:20:36 +0100

    labo1js

---
+++ JS/labo1/demo/scripts/code.js
@@ -0,0 +1,31 @@
+const setup = () => {
+    let lblCursus = document.getElementById('lblCursus');
+    lblCursus.addEventListener("mouseover", change);
+
+    let btnSend = document.getElementById('btnSend');
+    btnSend.addEventListener("click", show);
+}
+
+const change = () =>
+{
+    let lblCursus = document.getElementById('lblCursus');
+    lblCursus.className = 'cursus';
+}
+
+const show = () =>{
+    let txtName = document.getElementById('txtName');
+
+    if(txtName.value !== ""){
+        alert("Jouw naam is " + txtName.value);
+
+
+        console.log ("Jouw naam is " + txtName.value);
+
+        console.log(`Hij is ${txtName.value}`);
+    }
+    else{
+        alert("Gelieve naam in te vullen");
+    }
+}
+
+window.addEventListener("load", setup);
