e01f3030eece29bd32adb88ec894f7f2fbee99dd
Adrian-Jaron <adrian.jaron@student.vives.be>
2025-02-18 00:08:52 +0100

    sda

--- JS/labo2/uitdaging/scripts/code.js
+++ JS/labo2/uitdaging/scripts/code.js
@@ -4,9 +4,11 @@ button.addEventListener("click", function() {
     if(ul.style.listStyleType==="none"){
         ul.style.listStyleType="disc";
         button.innerHTML="No bullets";
+        ul.style.backgroundColor = "red";
     }
     else{
         ul.style.listStyleType="none";
         button.innerHTML="bullets";
+        ul.style.backgroundColor = null;
     }
 });
