5d526768b3f02c9209c83e31e312d74fb14e84dd
Adrian-Jaron <adrian.jaron@student.vives.be>
2025-04-01 10:29:07 +0200

    l

---
+++ JS/labo8/Date/Script/date.js
@@ -0,0 +1,14 @@
+const setup = () => {
+    let start = new Date('2025-04-01T12:10:30')
+    console.log(start);
+
+    console.log((start.getDay()));
+
+    console.log(start.getDate() + "-"
+        + (start.getMonth() + 1) + "-"
+        + start.getFullYear() + " " + start.getHours()
+        + ":" + start.getMinutes() + ":" + start.getSeconds());
+
+    con
+}
+
